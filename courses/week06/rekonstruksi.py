ca66d5b66ed4ecf371841ccba80d4927d8439e70
virgantara <vinux.edu@gmail.com>
2025-06-02 13:07:59 +0700

    Update

--- courses/week06/rekonstruksi.py
+++ courses/week06/rekonstruksi.py
@@ -4,34 +4,51 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
-from model import Autoencoder
+from model import Autoencoder, VAE
 from dataset import FashionMNISTDataset
 from tqdm import tqdm
+import argparse
 
-train_dataset = FashionMNISTDataset('./data/fashion-mnist/fashion-mnist_train.csv')
-train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
+def main(args):
+    train_dataset = FashionMNISTDataset('./data/fashion-mnist/fashion-mnist_train.csv')
+    train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = Autoencoder().to(device)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model.load_state_dict(torch.load('model.pth', weights_only=True))
-model.eval()
-with torch.no_grad():
-    test_imgs, _,_ = next(iter(train_loader))
-    test_imgs = test_imgs.to(device)
-    outputs = model(test_imgs)
+    if args.model_name == 'AE':
+        model = Autoencoder().to(device)
+    elif args.model_name == 'VAE':
+        model = VAE().to(device)
 
-    # Show original and reconstructed
-    n = 10
-    plt.figure(figsize=(20, 4))
-    for i in tqdm(range(n),desc="Hasil rekonstruksi"):
-        # original
-        ax = plt.subplot(2, n, i + 1)
-        plt.imshow(test_imgs[i].cpu().squeeze(), cmap='gray')
-        ax.axis("off")
-        
-        # reconstructed
-        ax = plt.subplot(2, n, i + 1 + n)
-        plt.imshow(outputs[i].cpu().squeeze(), cmap='gray')
-        ax.axis("off")
-    plt.show()
+    model.load_state_dict(torch.load('model_'+args.model_name+'.pth', weights_only=True))
+    model.eval()
+    with torch.no_grad():
+        test_imgs, _,_ = next(iter(train_loader))
+        test_imgs = test_imgs.to(device)
+
+        if args.model_name == 'AE':
+            outputs = model(test_imgs)
+        elif args.model_name == 'VAE':
+            outputs, _, _ = model(test_imgs)
+
+        # Show original and reconstructed
+        n = 10
+        plt.figure(figsize=(20, 4))
+        for i in tqdm(range(n),desc="Hasil rekonstruksi"):
+            # original
+            ax = plt.subplot(2, n, i + 1)
+            plt.imshow(test_imgs[i].cpu().squeeze(), cmap='gray')
+            ax.axis("off")
+            
+            # reconstructed
+            ax = plt.subplot(2, n, i + 1 + n)
+            plt.imshow(outputs[i].cpu().squeeze(), cmap='gray')
+            ax.axis("off")
+        plt.show()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_name', type=str, default='AE', help='Model Name')
+    args = parser.parse_args()
+    main(args)
